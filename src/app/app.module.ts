cb660fdbaac7bc535f12adab6e87ff453868397a
Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>
2021-01-02 00:37:49 -0500

    Config Inicial
    Signed-off-by: Kenny Saavedra Altuna <kennysaavedraaltuna@outlook.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PrimengModule } from './primeng/primeng.module';
+import { HttpClientModule } from '@angular/common/http';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { LoginComponent } from './components/login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    SideBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PrimengModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
